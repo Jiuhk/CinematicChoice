5653c22703c71588dc81724dff63276f49b3adb6
Carrie Shen <carrieshen.uk@gmail.com>
2023-09-10 20:30:02 +0100

    add modal feature to stills

--- components/common/stillsContainer/StillsContainer.jsx
+++ components/common/stillsContainer/StillsContainer.jsx
@@ -1,17 +1,21 @@
-import { ScrollView, Image } from "react-native";
+import { ScrollView, View, Image, Modal, TouchableOpacity } from "react-native";
 import { images } from "../../../constants";
+import { useState } from "react";
 
 import styles from "./stillscontainer.style";
 
-const StillsContainer = ( ) => {
-    return (
-        <ScrollView style={styles.reviewContainer} >
-            <Image style={styles.image} source={images.stills1} />
-            <Image style={styles.image} source={images.stills2} />
-            <Image style={styles.image} source={images.stills3} />
-            <Image style={styles.image} source={images.stills4} />
-        </ScrollView>
+const StillsContainer = ({ image }) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
+    return (
+        <TouchableOpacity onPress={() => { setIsModalVisible(!isModalVisible) }}>
+            <Image style={styles.image} source={images[image]} />
+            <Modal visible={isModalVisible} transparent={false} animationType="slide">
+                <TouchableOpacity onPress={() => { setIsModalVisible(!isModalVisible) }}  style={styles.modalContainer}>
+                    <Image style={styles.modalImage} source={images[image]} />
+                </TouchableOpacity>
+            </Modal>
+        </TouchableOpacity>
     )
 }
 
